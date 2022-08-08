84baf949709b3af77855c6fbacee84afbf6bef6c
annarow <57880374+annarow@users.noreply.github.com>
2022-08-08 12:12:12 -0700

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "the size of int is " << sizeof(int) << endl;
+	cout << "the size of double is " << sizeof(double) << endl;
+	cout << "the size of char is " << sizeof(char) << endl;
+	cout << "the size of bool is " << sizeof(bool) << endl;
+	cout << "the size of float is " << sizeof(float) << endl;
+	return 0;
+
+}
